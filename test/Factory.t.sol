7f42b401dc1ae8c9fc36a8c9cfe89354b7d7232f
0xSolDev <narayan@nuts.finance>
2024-12-12 11:41:47 +0530

    feat: check balance in test

--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.8.28;
 
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.25 <0.9.0;
-
 import { Test } from "forge-std/Test.sol";
 import { Vm } from "forge-std/Vm.sol";
 import { console } from "forge-std/console.sol";
 
 import { StableAssetFactory } from "../src/StableAssetFactory.sol";
 import { MockToken } from "../src/mock/MockToken.sol";
+import { StableAsset } from "../src/StableAsset.sol";
+import { LPToken } from "../src/LPToken.sol";
+import { WLPToken } from "../src/WLPToken.sol";
 
-contract FooTest is Test {
+contract FactoryTest is Test {
     StableAssetFactory internal factory;
     address governance = address(0x01);
-    address initialMiner = address(0x02);
+    address initialMinter = address(0x02);
 
     function setUp() public virtual {
         factory = new StableAssetFactory();
@@ -27,7 +27,7 @@ contract FooTest is Test {
         StableAssetFactory.CreatePoolArgument memory arg = StableAssetFactory.CreatePoolArgument({
             tokenA: address(tokenA),
             tokenB: address(tokenB),
-            initialMinter: address(initialMiner),
+            initialMinter: address(initialMinter),
             tokenBType: StableAssetFactory.TokenBType.Standard,
             tokenBOracle: address(0),
             tokenBFunctionSig: ""
@@ -46,13 +46,30 @@ contract FooTest is Test {
             Vm.Log memory log = entries[i];
 
             if (log.topics[0] == eventSig) {
-                console.logBytes(log.data);
                 (decodedPoolToken, decodedStableAsset, decodedWrappedPoolToken) =
                     abi.decode(log.data, (address, address, address));
-                console.log("Pool Token:", decodedPoolToken);
-                console.log("Stable Asset:", decodedStableAsset);
-                console.log("Wrapped Pool Token:", decodedWrappedPoolToken);
             }
         }
+
+        StableAsset stableAsset = StableAsset(decodedStableAsset);
+        LPToken poolToken = LPToken(decodedPoolToken);
+        WLPToken wrappedPoolToken = WLPToken(decodedWrappedPoolToken);
+
+        vm.startPrank(initialMinter);
+        tokenA.mint(initialMinter, 100e18);
+        tokenB.mint(initialMinter, 100e18);
+
+        tokenA.approve(address(stableAsset), 100e18);
+        tokenB.approve(address(stableAsset), 100e18);
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 100e18;
+        amounts[1] = 100e18;
+
+        vm.warp(block.timestamp + 1000);
+
+        stableAsset.mint(amounts, 0);
+
+        assertEq(poolToken.balanceOf(initialMinter), 200e18);
     }
 }
